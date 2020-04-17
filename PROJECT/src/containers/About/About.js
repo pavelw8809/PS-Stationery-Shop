c6d4b9d6c897629d81d0f8b2eee2575248a19dc2
Beata Czaja <beata.czaja963@gmail.com>
2020-04-17 22:04:27 +0200

    version_7.1

--- PROJECT/src/containers/About/About.js
+++ PROJECT/src/containers/About/About.js
@@ -12,8 +12,25 @@ import React from 'react';
 const about = () => {
     return (
         <div>
-            <h1>ABOUT</h1>
-            <p>ss</p>
+            <h1>o Firmie</h1>
+            <p>
+            PAPER&SCISORS to firma, która z powodzeniem działa na rynku od ponad 5 lat. 
+            </p>
+            <p>
+             Przez cały ten okres budowaliśmy naszą markę w oparciu o rzetelność i rozumienie potrzeb naszych klientów.
+            </p>
+            <p>
+            W naszej ofercie znajdą Państwo artykuły papiernicze, artykuły biurowe, koperty oraz artykuły piśmiennicze. Wszystkie nasze produkty cechują się najwyższą jakością oraz spełniają standardy ochrony środowiska i bezpieczeństwa pracy.
+            </p>
+            <p>
+            Mając na uwadze jak ważną rolę pełni szybka dostawa towaru, oferujemy usługę dostarczenia lub odbioru produktów już w dniu zamówienia. Jest to możliwe dzięki odpowiedniemu zaopatrzeniu naszych stanów magazynowych.
+            </p>
+            <p>
+            Doświadczenie pozwoliło nam zdobyć zaufanie i uznanie naszych klientów. Spostrzeżenia naszych użytkowników oraz ich sugestie stanowią dla nas fundament kształtowania oferty i umożliwiają nam zbudowanie wyselekcjonowanego zbioru produktów o najwyższej jakości.
+            </p>
+            <p>
+            Wychodząc naprzeciw oczekiwaniom naszych klientów cały czas zwiększamy asortyment produktów oraz dokonujemy usprawnień sklepu internetowego. Jednocześnie gorąco zachęcamy do zgłaszania Państwa pomysłów oraz propozycji, które pozwolą nam na jeszcze sprawniejszą obsługę.
+            </p>
         </div>
     )
 }
