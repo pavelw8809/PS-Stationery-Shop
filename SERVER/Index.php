07fa27345ca9f0c406d5fff99af71180a85637d9
Beata Czaja <beata.czaja963@gmail.com>
2020-05-11 22:34:10 +0200

    version 10.5

---
+++ SERVER/Index.php
@@ -0,0 +1,23 @@
+<?php
+
+    include('server.php');
+
+    header('Access-Control-Allow-Methods: GET');
+    header("Access-Control-Allow-Headers: X-Requested-With");
+
+    $con = new mysqli($dbserv, $dbuser, $dbpass, $dbname);
+    $con -> set_charset("utf8");
+
+    if ($con -> connect_error) {
+        die("Connection error");
+    }
+
+    $sqlquery = "SELECT distinct * FROM products p 
+    left join order_details od on p.p_id=od.od_o_id 
+    ORDER BY od.od_quality DESC LIMIT 5";
+    $result = mysqli_query($con, $sqlquery);
+    while ($r = mysqli_fetch_assoc($result)) {
+        $output[] = $r;
+    }
+
+    echo json_encode($output);
